11fb1018c6aa31553b76d2c42841e9b2095f2942
danielfc <daniel.felicissimo@gmail.com>
2016-12-14 14:13:51 -0200

    Add women's endpoint

--- superliga-scrapper/README.md
+++ superliga-scrapper/README.md
@@ -4,17 +4,33 @@
 
 ## Usage
 
-`$ node index.js`
+- `$ npm install`
+- `$ node index.js`
 
-`$ curl http://localhost:3000/games`
+## Endpoints
+
+`$ curl http://localhost:3000/men/schedule`
 
 ```json
 [{  
    "id":"249ffa20-c14f-11e6-859d-43475c136a54",
    "round":10,
-   "date":"17/12/2016 | 19:30",
+   "date":"2016-12-17 19:30",
    "tv":"SportTV",
    "home":"SADA CRUZEIRO VÔLEI",
    "away":"FUNVIC TAUBATÉ"
+}]
+```
+
+`$ curl http://localhost:3000/women/schedule`
+
+```json
+[{
+  "id": "cad43500-c217-11e6-9ca6-ebbf66ff569a",
+  "round": 1,
+  "date": "2016-10-27 19:00",
+  "tv": null,
+  "home": "RENATA VALINHOS / COUNTRY ",
+  "away": "DENTIL/PRAIA CLUBE"
 }]
 ```
--- superliga-scrapper/index.js
+++ superliga-scrapper/index.js
@@ -1,47 +1,68 @@
-let cheerio = require('cheerio');
-let express = require('express');
-let request = require('request');
-let uuid = require('uuid/v1');
-let app = express();
+var cheerio = require('cheerio');
+var express = require('express');
+var request = require('request');
+var uuid = require('uuid/v1');
+var moment = require('moment');
 
-app.get('/games', function(req, res) {
-    var url = "http://superliga.cbv.com.br/tabela-jogos/Masculino";
+var app = express();
 
-    request(url, function(error, response, html) {
-        if (error) {
-            return res.sendError(`Failed to access ${url}`);
-        }
+var getSchedule = function(url) {
+    return new Promise(function(resolve, reject) {
+        request(url, function(error, response, html) {
+            if (error) {
+                reject(`Failed to access ${url}`);
+            }
 
-        var games = [];
-        var $ = cheerio.load(html);
+            var games = [];
+            var $ = cheerio.load(html);
 
-        $('.titulo-rodada').each(function(i, elem) {
-            var round = i + 1;
-            $(this).nextUntil('.titulo-rodada').each(function(i, elem) {
-                var date = $(this).find('.data').last().html().split('<br>')[0];
-                var img = $(this).find('.data').last().children('img');
-                var tv = null;
-                if (img.length > 0) {
-                    tv = img.attr('src').indexOf('sportv') > 0 ? 'SportTV' : 'RedeTV';
-                }
-                var home = $(this).find('.equipes').children().first().children().last().text();
-                var away = $(this).find('.equipes').children().last().children().last().text();
+            $('.titulo-rodada').each(function(i, elem) {
+                var round = i + 1;
+                $(this).nextUntil('.titulo-rodada').each(function(i, elem) {
+                    var rawDate = $(this).find('.data').last().html().split('<br>')[0];
+                    var date = moment(rawDate, 'DD/MM/YYYY | HH:mm').format('YYYY-MM-DD HH:mm');
+                    var img = $(this).find('.data').last().children('img');
+                    var tv = null;
+                    if (img.length > 0) {
+                        tv = img.attr('src').indexOf('sportv') > 0 ? 'SportTV' : 'RedeTV';
+                    }
+                    var home = $(this).find('.equipes').children().first().children().last().text();
+                    var away = $(this).find('.equipes').children().last().children().last().text();
 
-                games.push({
-                    id: uuid(),
-                    round: round,
-                    date: date,
-                    tv: tv,
-                    home: home,
-                    away: away
+                    games.push({
+                        id: uuid(),
+                        round: round,
+                        date: date,
+                        tv: tv,
+                        home: home,
+                        away: away
+                    });
                 });
             });
+
+            console.log(JSON.stringify(games));
+
+            resolve(games);
         });
-
-        console.log(JSON.stringify(games));
-
     });
+}
 
+app.get('/men/schedule', function(req, res) {
+    getSchedule("http://superliga.cbv.com.br/tabela-jogos/Masculino")
+        .then(function(schedule) {
+            res.send(schedule);
+        }).catch(function(error) {
+            res.sendError(error);
+        });
+});
+
+app.get('/women/schedule', function(req, res) {
+    getSchedule("http://superliga.cbv.com.br/tabela-jogos/Feminino")
+        .then(function(schedule) {
+            res.send(schedule);
+        }).catch(function(error) {
+            res.sendError(error);
+        });
 });
 
 app.listen(3000, function() {
