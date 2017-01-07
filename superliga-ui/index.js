375478fb4ca9f031eff9bd989450736505e0ef53
danielfc <daniel.felicissimo@gmail.com>
2017-01-07 12:17:54 -0200

    Configure simple express to serve static files

---
+++ superliga-ui/index.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('.'));
+
+app.listen(process.env.PORT || 3001, function() {
+    console.log('Service running on port ' + (process.env.PORT || 3001));
+});
